cadeb7101f7d9beda73ef8f67ee7d353642db95c
shamloor <1301585528@qq.com>
2024-03-11 16:08:12 +0800

    add random test

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -31,8 +31,61 @@ static char *code_format =
 "  return 0; "
 "}";
 
+static uint32_t choose(uint32_t n) {
+	/* to generate a 
+	 * different rand number in one second. */
+	return rand() % n;
+} 
+
+//check division before zero, can just exclude literal division.
+static bool check_legal_zero() {
+	int pos = strlen(buf) - 1;
+
+	if (buf[pos] == '/') {
+		return false;
+	} else if (buf[pos] == ' ') {
+		return check_legal_zero();
+	}
+	return true;
+}
+
+static void gen_num() {
+	char *tmp = {};
+	int digit = rand() % 100;
+	if (digit == 0) {
+		if(!check_legal_zero()) {
+			gen_num();
+			return;
+		}
+	}
+	sprintf(tmp, "%d", digit);
+	strcat(buf, tmp);
+}
+
+static void gen(char *str) {
+	strcat(buf, str);
+}
+
+static void gen_rand_op() {
+	if (strlen(buf) > 65500) {
+		return;
+	}
+	switch(choose(4)) {
+		case 0: gen("+"); break;
+		case 1: gen("-"); break;
+		case 2: gen("*"); break;
+		default: gen("/"); break;
+	}
+}
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+	if (buf)
+	switch(choose(4)) {
+		case 0: gen_num(); break;
+		case 1: gen("("); gen_rand_expr(); gen(")"); break;
+		case 2: gen(" ");
+		default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;	
+	}
 }
 
 int main(int argc, char *argv[]) {
@@ -40,26 +93,30 @@ int main(int argc, char *argv[]) {
   srand(seed);
   int loop = 1;
   if (argc > 1) {
+		// read from argv[1] to loop variable.
     sscanf(argv[1], "%d", &loop);
   }
   int i;
   for (i = 0; i < loop; i ++) {
     gen_rand_expr();
-
+		// insert buf to code_fromat and store them in code_buf.
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
     fputs(code_buf, fp);
     fclose(fp);
-
+		
+		// shell command, return 0 if success.
     int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
-
+		
+		// opens a process, readable.
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
 
     int result;
+		// read from stream fp, store them in result.
     ret = fscanf(fp, "%d", &result);
     pclose(fp);
 
